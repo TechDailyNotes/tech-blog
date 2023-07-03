8a1e563f71a20404354d40d19e637edbe5285004
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-07-03 11:29:41 +0800

    Update Redis.py

--- pages/Redis.py
+++ pages/Redis.py
@@ -3,7 +3,7 @@ from utils import read_markdown_file
 
 st.header("Redis")
 
-tab1, tab2, tab3 = st.tabs(["Basics", "Scenarios", "Theories"])
+tab1, tab2, tab3 = st.tabs(["Concepts", "Scenarios", "Theories"])
 
 with tab1:
     st.markdown(read_markdown_file("redisConcepts.md"))
