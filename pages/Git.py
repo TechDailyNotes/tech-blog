b23b19fc5d4642153ced3894cef318d0ae09d450
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-07-03 17:33:25 +0800

    Update Git.py

--- pages/Git.py
+++ pages/Git.py
@@ -6,7 +6,7 @@ st.header("Git")
 tab1, tab2 = st.tabs(["Branch", "Cherry Pick"])
 
 with tab1:
-  st.markdown(read_markdown_file("gitCommands.md"))
+  st.markdown(read_markdown_file("gitBranches.md"))
 
 with tab2:
   st.markdown(read_markdown_file("gitCherryPick.md"))
