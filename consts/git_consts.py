ab66f136e9a6fca73d2d268d9f5a59846c891198
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-07-03 19:17:43 +0800

    remove git cherry-pick page

--- consts/git_consts.py
+++ consts/git_consts.py
@@ -1,4 +1,4 @@
 GIT_COMMANDS_TO_FILE_PATHS = {
-    "git branch": "gitBranch.md",
-    "git cherry-pick": "gitCherryPick.md"
+    "git branch": "gitBranch.md"
+    # "git cherry-pick": "gitCherryPick.md"
 }
