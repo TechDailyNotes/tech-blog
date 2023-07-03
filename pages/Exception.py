9f7a09def6b55f6beb339b326a3bee77b8e6395f
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-07-04 00:51:53 +0800

    add exceptions

---
+++ pages/Exception.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from utils import read_markdown_file
+
+st.header("Exception")
+
+tab1 = st.tabs(["Questions"])
+
+tab1[0].markdown(read_markdown_file("exceptionQuestions.md"))
