5895dabfe044b77c16e56dd44b4b90f49fd3666d
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-07-03 19:10:10 +0800

    update git commands webpage structure

---
+++ consts/git_consts.py
@@ -0,0 +1,4 @@
+GIT_COMMANDS_TO_FILE_PATHS = {
+    "git branch": "gitBranch.md",
+    "git cherry-pick": "gitCherryPick.md"
+}
---
+++ pages/Git_Commands.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from utils import read_markdown_file
+from consts.git_consts import GIT_COMMANDS_TO_FILE_PATHS
+
+st.header("Git Commands")
+
+command_name = st.selectbox(
+    "", options=GIT_COMMANDS_TO_FILE_PATHS.keys(), index=0, label_visibility="collapsed")
+
+st.divider()
+
+st.markdown(read_markdown_file(GIT_COMMANDS_TO_FILE_PATHS[command_name]))
