9230fc98d35d5eb47e4d2fd0eb6cc2433a06bad6
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-07-04 00:07:38 +0800

    update Annos constants

--- consts/anno_consts.py
+++ consts/anno_consts.py
@@ -2,4 +2,15 @@ LIBRARIES_TO_FILE_PATHS = {
     "Mockito": "annoMockito"
 }
 
-TABS_TITLE = ["Concepts", "Annos", "Questions"]
+TABS_TITLE = [
+    {
+        "tab-name": "Concepts",
+        "file-name": "Concepts"
+    }, {
+        "tab-name": "Annotations",
+        "file-name": "Annos"
+    }, {
+        "tab-name": "Questions",
+        "file-name": "Questions"
+    }
+]
--- pages/Annotations.py
+++ pages/Annotations.py
@@ -10,10 +10,11 @@ st.header("Annotations")
 library_name = st.selectbox("Library", options=LIBRARIES_TO_FILE_PATHS.keys(
 ), index=0, label_visibility="collapsed")
 
+tab1, tab2, tab3 = st.tabs([title['tab-name'] for title in TABS_TITLE])
+
+i = 0
 file_path_prefix = LIBRARIES_TO_FILE_PATHS[library_name]
-
-tab1, tab2, tab3 = st.tabs(TABS_TITLE)
-
-tab1.markdown(read_markdown_file(file_path_prefix + TABS_TITLE[0] + ".md"))
-tab2.markdown(read_markdown_file(file_path_prefix + TABS_TITLE[1] + ".md"))
-tab3.markdown(read_markdown_file(file_path_prefix + TABS_TITLE[2] + ".md"))
+for tab in [tab1, tab2, tab3]:
+    tab.markdown(read_markdown_file(file_path_prefix +
+                 TABS_TITLE[i]['file-name'] + ".md"))
+    i += 1
