fe6a64c45a52a789c69897c8e164f2a7753cebbe
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-07-04 23:07:39 +0800

    init soa page

---
+++ pages/SOA.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from utils import (
+    display_default_select_box,
+    read_markdown_file
+)
+from consts.soa_consts import (
+    HEADER,
+    SELECTBOX_TITLE,
+    FILE_PATH_PREFIX,
+    SELECTBOX_OPTION_TO_FILE_PATHS
+)
+
+st.header(HEADER)
+
+option_title = display_default_select_box(
+    SELECTBOX_TITLE, SELECTBOX_OPTION_TO_FILE_PATHS)
+
+st.divider()
+
+file_suffix = SELECTBOX_OPTION_TO_FILE_PATHS[option_title]
+st.markdown(read_markdown_file(FILE_PATH_PREFIX + file_suffix))
