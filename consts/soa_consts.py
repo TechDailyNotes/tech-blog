1de450baf7ebb47f60015ca22c9a163c6a595c3b
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-07-05 00:44:53 +0800

    add Dubbo

--- consts/soa_consts.py
+++ consts/soa_consts.py
@@ -6,5 +6,6 @@ FILE_PATH_PREFIX = "soa"
 
 SELECTBOX_OPTION_TO_FILE_PATHS = {
     "Concepts": "Concepts",
-    "Development": "Dev"
+    "Development": "Dev",
+    "Dubbo": "Dubbo"
 }
---
+++ docs/soaDubbo.md
@@ -0,0 +1,7 @@
+#### 发展历程
+
+```markdown
+<!-- 1. Architecture -->
+
+ORM -> MVC -> RPC -> SOA
+```
