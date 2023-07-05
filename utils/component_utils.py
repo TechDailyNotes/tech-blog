33543e1881bd5215c89e114642dc539e56eaa4f1
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-07-05 13:39:22 +0800

    update method call and add parameters names

--- utils/component_utils.py
+++ utils/component_utils.py
@@ -1,7 +1,10 @@
 import streamlit as st
 
 
-def display_default_select_box(selectbox_title: str, selectbox_title_to_file_paths: str) -> str:
+def display_default_select_box(
+        selectbox_title: str,
+        selectbox_title_to_file_paths: str
+) -> str:
     return st.selectbox(selectbox_title,
                         options=selectbox_title_to_file_paths.keys(),
                         index=0, label_visibility="collapsed")
--- utils/page_utils.py
+++ utils/page_utils.py
@@ -8,19 +8,18 @@ def display_default_page(constants_src: Constant) -> None:
     from utils.const_utils import import_constants
     from utils.doc_utils import read_markdown_doc
 
-    consts_dict = import_constants(constants_src)
+    consts_dict = import_constants(constants_src=constants_src)
 
     st.header(consts_dict["HEADER"])
 
     option_title = display_default_select_box(
-        consts_dict["SELECTBOX_TITLE"],
-        consts_dict["SELECTBOX_OPTION_TO_FILE_PATHS"]
+        selectbox_title=consts_dict["SELECTBOX_TITLE"],
+        selectbox_title_to_file_paths=consts_dict["SELECTBOX_OPTION_TO_FILE_PATHS"]
     )
 
     st.divider()
 
     file_suffix = consts_dict["SELECTBOX_OPTION_TO_FILE_PATHS"][option_title]
     st.markdown(read_markdown_doc(
-        consts_dict["FILE_PATH_PREFIX"] +
-        file_suffix
+        markdown_doc_name=consts_dict["FILE_PATH_PREFIX"] + file_suffix
     ))
