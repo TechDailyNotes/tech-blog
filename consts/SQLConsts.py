92a1835dfd11cdace09698338e55a33069e41169
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-07-09 09:15:06 +0800

    int sql pages

---
+++ consts/SQLConsts.py
@@ -0,0 +1,12 @@
+from consts.Consts import Consts
+
+
+class SQLConsts(Consts):
+    def __init__(self):
+        self.HEADER = "SQL Tuning"
+        self.SELECTBOX_TITLE = "Methods"
+        self.FOLDER_NAME = "sql/"
+        self.FILE_PATH_PREFIX = "sql"
+        self.SELECTBOX_OPTION_TO_FILE_PATHS = {
+            "Hints": "Hints"
+        }
